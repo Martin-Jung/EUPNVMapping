5d48b99650add3b76d9526bc06936d1c4a780dab
Martin Jung <martinjung@zoho.com>
2025-01-15 15:53:07 +0100

    Updated ensemble calculations for EBV portal

--- 05_postprocessPNV.R
+++ 05_postprocessPNV.R
@@ -18,6 +18,9 @@ dir.create(path_ensemble, showWarnings = FALSE)
 
 path_output = paste0("/mnt/hdrive/","PNVHabitats__ClimateRun/")
 
+# Adjust directory
+terra::terraOptions(tempdir = "/media/martin/AAB4A0AFB4A08005/tmp/")
+
 # -------------------- #
 # Get reference raster for the given grain
 background <- terra::rast( "data/background_ref.tif") 
@@ -171,8 +174,8 @@ for(vars in unique(df$variable)){
             )
             dir.create(ofg, showWarnings = FALSE,recursive = TRUE)
             # Build output name
-            ofname <- paste0(ofg, "/", lubridate::year(terra::time(out)), ".tif")
-            if(file.exists(ofname)) next()
+            ofname <- paste0(ofg, "/", lubridate::year(terra::time(ras)[1]), ".tif")
+            if(file.exists(ofname)) { next() }
             
             message(vars, "  -  " , n)
             out <- terra::weighted.mean(ras[[which(names(ras)==n)]],w = w$value)
@@ -194,3 +197,156 @@ for(vars in unique(df$variable)){
     }
   }
 }
+
+# --- #
+# List all items in the directory and filter to include only folders
+folders <- list.dirs(path_ensemble, full.names = TRUE, recursive = FALSE)
+
+# Loop through each folder and create a zip file
+for (folder in folders) {
+  # Get the folder name
+  folder_name <- basename(folder)
+  
+  # Define the output zip file path
+  zip_file <- file.path(path_ensemble, paste0(folder_name, ".zip"))
+  
+  # Create the zip file
+  zip(zipfile = zip_file, files = list.files(folder, full.names = TRUE),)
+  
+  cat("Zipped folder:", folder, "->", zip_file, "\n")
+}
+
+# ---------------- #
+#### Calculate ensemble projections per SSP ####
+# Here we simply collate all layers and calculate an ensemble per ssp
+# averaging across GCMs
+
+# Get all projections for ensemble calculations
+ll <- list.files(path_output, full.names = TRUE, recursive = TRUE)
+ll <- ll[has_extension(ll,"tif")]
+ll <- ll[grep("Projection", ll)]
+assertthat::assert_that(length(ll)>0, msg = "No results found...")
+
+# Make a container of all projections
+df <- data.frame(ifname = ll, split = tools::file_path_sans_ext(basename(ll))) |> 
+  tidyr::separate(split,sep = "__",into = c("Proj", "variable", "model", "timeperiod", "ssp", "gcm")) |> 
+  dplyr::select(-Proj)
+
+# Apply mask directly?
+apply_mask <- TRUE
+if(apply_mask) assertthat::assert_that(exists("bb"))
+
+# Create ensembles
+for(vars in unique(df$variable)){
+    for(s in unique(df$ssp)){
+      for(p in unique(df$timeperiod)){
+        
+        sub <- df |> dplyr::filter(variable == {{vars}},ssp == {{s}}, 
+                                   timeperiod == {{p}})
+        # Get the weights too
+        w <- vals |> dplyr::filter(variable == {{vars}}) |> 
+          # Aggregate per model 
+          dplyr::filter(metric == "f1") |> dplyr::group_by(model) |> 
+          dplyr::summarise(value = mean(value))
+        sub <- dplyr::left_join(sub, w)
+
+        # Load both rasters and average
+        ras <- terra::rast(sub$ifname)
+        # If raster lacks time, add here.
+        if(is.na(terra::time(ras)[1])) terra::time(ras) <- rep(as.Date(as.numeric(p)), terra::nlyr(ras))
+        
+        for(n in unique(names(ras)) ){
+          if(n %in% c("suitability_q05","suitability_q50","suitability_q95","suitability_mean")){
+            # Now create folder structure and save
+            # Scenario / metric /entity / time
+            # Build output folder
+            ofg <- paste0(path_ensemble, "/", 
+                          paste0(s), # Scenario
+                          "/",
+                          paste0(n), # Metric
+                          "/",
+                          paste0(tolower(vars)) # Entity
+            )
+            dir.create(ofg, showWarnings = FALSE,recursive = TRUE)
+            # Build output name
+            ofname <- paste0(ofg, "/", lubridate::year(terra::time(ras)[1]), ".tif")
+            if(file.exists(ofname)) { next() }
+            
+            message(vars, "  -  " , n)
+            out <- terra::weighted.mean(ras[[which(names(ras)==n)]],w = sub$value)
+            names(out) <- n
+            if(apply_mask){
+              # Crop
+              out <- terra::crop(out, bb)
+              out <- terra::extend(out, bb)
+              # Now mask
+              out <- terra::mask(out, bb)
+            }
+            if(is.na(terra::time(out))){
+              terra::time(out) <- terra::time(ras)[1]
+            } 
+            terra::writeRaster(out, ofname, datatype = "FLT4S",overwrite = TRUE)
+          }
+        }
+      }
+    }
+}
+
+# --- #
+# Also create/copy the starting year in there calculated as ensemble.
+# Check results
+ll <- list.files(path_output, full.names = TRUE, recursive = TRUE)
+ll <- ll[has_extension(ll,"tif")]
+ll <- ll[grep("Prediction", ll)]
+assertthat::assert_that(length(ll)>0, msg = "No results found...")
+
+ll_ex <- list.dirs(path_ensemble, full.names = TRUE, recursive = TRUE)
+assertthat::assert_that(length(ll_ex)>0)
+
+# And validation statistics
+ll_val <- list.files(path_output, full.names = TRUE, recursive = TRUE)
+ll_val <- ll_val[has_extension(ll_val,"rds")]
+ll_val <- ll_val[grep("Fold", ll_val)]
+
+# Function to read in validation results
+vals <-
+  ll_val %>% 
+  purrr::map_df(~readRDS(.) |> dplyr::mutate(filename = basename(.)))
+# Format filename
+vals <- tidyr::separate(vals, filename, c("Valid", "variable", "model", "FoldRepeat"),sep = "__") |> 
+  # Drop NA (old validation results)
+  tidyr::drop_na(FoldRepeat)
+
+# Create ensembles
+for(s in c("ssp126", "ssp370", "ssp585")){
+  for(vars in unique(vals$variable)){
+    ras <- terra::rast( grep(vars,ll,ignore.case = TRUE,value = TRUE) )
+    w <- vals |> dplyr::filter(variable == vars) |> 
+      # Aggregate per model 
+      dplyr::filter(metric == "f1") |> dplyr::group_by(model) |> 
+      dplyr::summarise(value = mean(value)) |> dplyr::pull(value)
+    
+    # Subset to relevant entries and rename
+    ras <- ras[[grep(pattern = "mean|q05|q50|q95", names(ras))]]
+    for(n in unique(names(ras))){
+      message(vars, "-", n)
+      out <- terra::weighted.mean(ras[[which(names(ras)==n)]],w = w)
+      names(out) <- switch (n,
+        "mean" = "suitability_mean",
+        "q05" = "suitability_q05",
+        "q50" = "suitability_q50",
+        "q95" = "suitability_q95"
+      )
+      # Now write to each folder a copy
+      ofg <- grep(names(out),ll_ex, value = TRUE)
+      ofg <- grep(tolower(make.names(vars)), ofg, value = TRUE)
+      assertthat::assert_that(length(ofg)==3)
+      
+      for(k in ofg){
+        writeRaster(out, paste0(k, "/2020.tif"), datatype = "FLT4S", overwrite = TRUE)
+      }
+    }
+  }
+}
+gc(verbose = TRUE)
+
